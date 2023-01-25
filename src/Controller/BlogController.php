885f21e6bd02be82b0e003456d9fa0f5a6ff4212
Monique Garcia Dutaitre <formationwebmg@gmail.com>
2023-01-25 18:21:21 +0100

    maj erreur sur blogcontroller

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -1,7 +1,8 @@
+<?php
 // src/Controller/BlogController.php
 namespace App\Controller;
 
-
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
